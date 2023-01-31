59a047d38fa9e54ec0df5533d106b398177c3171
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2023-01-31 21:10:06 +0530

    Update index.js

--- src/components/LanguageFilterItem/index.js
+++ src/components/LanguageFilterItem/index.js
@@ -2,11 +2,21 @@ import {Component} from 'react'
 import './index.css'
 
 class LanguageFilterItem extends Component {
+  onClickLanguageFilterItem = () => {
+    const {itemDetails, updateActiveLanguageId} = this.props
+    const {id} = itemDetails
+    updateActiveLanguageId(id)
+  }
+
   render() {
     const {itemDetails} = this.props
-    const {id, language} = itemDetails
+    const {language} = itemDetails
     return (
-      <button type="button" className="language-filter-item-btn">
+      <button
+        type="button"
+        className="language-filter-item-btn"
+        onClick={this.onClickLanguageFilterItem}
+      >
         {language}
       </button>
     )
